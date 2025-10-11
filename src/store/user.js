2985367a4b804d748e3f46648a1b309aba44f7f9
unknown <sm.sim.2024@computing.smu.edu.sg>
2025-10-11 20:12:01 +0800

    reactive navbar

--- src/store/user.js
+++ src/store/user.js
@@ -1,13 +1,37 @@
-import { reactive } from "vue";
+import { reactive } from "vue"
+import { auth, onAuthStateChanged } from "../firebase/firebase_config"
 
 export const user = reactive({
   isLoggedIn: false,
-  name: "Mirabelle",
-  avatar: "https://i.pravatar.cc/40",
-  cart: ["item1", "item2"],
-  wishlist: ["itemA"],
+  name: null,
+  avatar: null,
+  uid: null,
+  email: null,
+  cart: [],
+  wishlist: [],
   preferences: {
     theme: "dark",
-    language: "en"
+    language: "en",
+  },
+})
+
+// Listen for login/logout changes automatically
+onAuthStateChanged(auth, (firebaseUser) => {
+  if (firebaseUser) {
+    // Logged in
+    user.isLoggedIn = true
+    user.name = firebaseUser.displayName || "Anonymous"
+    user.avatar = firebaseUser.photoURL || "../../public/avatar.png"
+    user.uid = firebaseUser.uid
+    user.email = firebaseUser.email
+  } else {
+    // Logged out
+    user.isLoggedIn = false
+    user.name = null
+    user.avatar = null
+    user.uid = null
+    user.email = null
+    user.cart = []
+    user.wishlist = []
   }
-});
+})
